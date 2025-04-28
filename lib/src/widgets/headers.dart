1b22f890b2c97607fe0dbfec45a4194eafada196
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2025-04-28 15:00:26 -0600

    Headers Diagonal

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class HeadersCuadrado extends StatelessWidget {
@@ -24,10 +22,48 @@ class HeadersBordesRendondeados extends StatelessWidget {
       decoration: BoxDecoration(
         color: Color(0xFF615AAB), // Color aquí dentro
         borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(70),
-          bottomRight: Radius.circular(70)
-        ),
+            bottomLeft: Radius.circular(70), bottomRight: Radius.circular(70)),
       ),
     );
+  }
+}
+
+class HeadersDiagonal extends StatelessWidget {
+  const HeadersDiagonal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeadersDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeadersDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    //Propiedades
+    paint.color = Color(0xFF615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;
+
+    final path = Path();
+    // Dibujar en el path
+    path.moveTo(0, size.height * .35);
+    path.lineTo(size.width, size.height * .20);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
