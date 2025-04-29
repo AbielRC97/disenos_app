64913ce1fe5ae00b6e20ab015ddde8b4e47395ae
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2025-04-28 21:58:11 -0600

    android/app/.cxx/

---
+++ lib/src/pages/animation_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class AnimationPage extends StatelessWidget {
+  const AnimationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _CuadradoAnimado(),
+      ),
+    );
+  }
+}
+
+class _CuadradoAnimado extends StatefulWidget {
+  @override
+  State<_CuadradoAnimado> createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<_CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotacion;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 4000));
+    rotacion = Tween(begin: 0.0, end: 2 * 3.1416).animate(controller);
+    controller.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        controller.reverse();
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    controller.forward();
+    return AnimatedBuilder(
+        animation: controller,
+        builder: (BuildContext ctx, Widget? child) {
+          return Transform.rotate(
+            angle: rotacion.value,
+            child: _Rectangulo(),
+          );
+        });
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(color: Colors.blue),
+    );
+  }
+}
