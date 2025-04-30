90a65e79e5ef7d3c0fe26bb1b740bd60e5287f2e
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2025-04-30 12:15:08 -0600

    Video 64:  Slide Widget

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -8,8 +8,36 @@ class SlidesShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svg/slide-1.svg'),
+        child: _Slides(),
       ),
     );
+  }
+}
+
+//SvgPicture.asset('assets/svg/slide-1.svg')
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        _Slide('assets/svg/slide-1.svg'),
+        _Slide('assets/svg/slide-2.svg'),
+        _Slide('assets/svg/slide-3.svg')
+      ],
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+  const _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(30),
+        width: double.infinity,
+        height: double.infinity,
+        child: SvgPicture.asset(svg));
   }
 }
