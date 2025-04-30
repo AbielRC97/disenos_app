61421d2c19ac3113fb9679b2d9ccfbe568baf3d3
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2025-04-30 11:57:51 -0600

    Video 61: Assets/SVG

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
+import 'package:disenos_app/src/pages/slideshow_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Diseños APP',
       debugShowCheckedModeBanner: false,
-      home: GraficasCircularesPage(),
+      home: SlidesShowPage(),
     );
   }
 }
---
+++ lib/src/pages/slideshow_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SlidesShowPage extends StatelessWidget {
+  const SlidesShowPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
